75762af9c23db6965052f17953c153d7d98cb21e
arty gvozdenkov <gvozdenkov@gmail.com>
2023-12-26 05:31:19 +0300

    chore: setup eslint, pretter, ts

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  ignorePatterns: ["build", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        //always try to resolve types under `<root>@types` directory
+        // even it doesn't contain any source code, like `@types/unist`
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+      node: {
+        extensions: [".ts", ".js", ".json"],
+      },
+    },
+    "import/extensions": [".js", ".ts"],
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+    semi: "error",
+    "no-var": 0,
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        ts: "never",
+      },
+    ],
+  },
+};
--- README.md
+++ README.md
@@ -5,3 +5,47 @@
 - Typescript
 - Mongodb
 - Node.js + Express
+
+## Workflow setup
+
+### Eslint
+
+Install deps:
+
+```bash
+yarn add -D eslint eslint-config-airbnb-base eslint-config-airbnb-typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-import-resolver-typescript eslint-plugin-import
+```
+
+### Prettier
+
+Install exact versions (I have some bugs with prettier 3)
+
+```bash
+yarn add -D prettier@2.8.7 eslint-plugin-prettier@4.2.1 eslint-config-prettier
+```
+
+Add to eslint config:
+
+```js
+module.exports = {
+  extends: [
+    // add at the end of array!
+    "pretter"
+  ],
+  plugins: {
+    "prettier"
+  },
+  rules: {
+    // highlight prettier errors
+    "prettier/prettier": ["error"],
+  }
+}
+```
+
+Add script in `package.json` fix prettier styles:
+
+```json
+"scripts": {
+  "prettier:write": "prettier --write ./**/*.{ts,js} ./*.{json,md,yml} -l",
+}
+```
