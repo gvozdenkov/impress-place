e86463539026bb7110f7515196ce2be22f328b70
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-16 12:49:50 +0300

    feat: connect mongodb in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express, { Application } from 'express';
-import { BASE_PATH, router } from '#api/v1';
+import cors from 'cors';
+import { BASE_PATH, connectDb, router } from '#api/v1';
 
 var app: Application = express();
 
-var PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+connectDb();
+
 app.use(BASE_PATH, router);
+
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
