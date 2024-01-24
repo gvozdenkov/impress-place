b2906e5087fe7bc47381143e21b2f75736463886
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-24 12:42:24 +0300

    refactor(user): separate db methods to module

--- src/api/v1/user/user.controller.ts
+++ src/api/v1/user/user.controller.ts
@@ -1,33 +1,28 @@
 import { NextFunction, Request, Response } from 'express';
-import { User, UserSchema } from './user.model';
+import { UserSchema } from './user.model';
 import { TypedRequestBody } from '../types';
 import { responseBuilder } from '../middlewares';
+import { userService } from './user.service';
 
 var createUser = async (req: TypedRequestBody<UserSchema>, res: Response, next: NextFunction) => {
   var { name, about, avatar } = req.body;
 
-  try {
-    var user = new User({ name, about, avatar });
-    var savedUser = await user.save();
-    res.status(200).json(responseBuilder('success', savedUser));
-  } catch (e) {
-    next(e);
-  }
+  userService
+    .create({ name, about, avatar })
+    .then((user) => res.status(200).json(responseBuilder('success', user)))
+    .catch(next);
 };
 
 var getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    var result = await User.find({});
-    res.send(result);
-  } catch (e) {
-    next();
-  }
+  userService
+    .getAll()
+    .then((users) => res.status(200).json(responseBuilder('success', users)))
+    .catch(next);
 };
 
 var getUserById = (req: Request, res: Response, next: NextFunction) => {
-  var id = req.params.userId;
-
-  User.findById(id)
+  userService
+    .getById(req.params.userId)
     .then((user) => res.status(200).json(responseBuilder('success', user)))
     .catch(next);
 };
