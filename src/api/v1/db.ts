d8a6df6310858a44325475ae82d206cd2809d6ac
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-24 09:18:30 +0300

    feat(db): switch db name with NODE_ENV from .env

--- src/api/v1/db.ts
+++ src/api/v1/db.ts
@@ -3,23 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export var dbName =
+  process.env.NODE_ENV === 'testing' ? process.env.DB_NAME_TEST : process.env.DB_NAME;
+
 var options = {
-  dbName: process.env.DB_NAME,
+  dbName,
 };
 
 export var connectDb = () => {
   mongoose
-    .connect(process.env.MONGODB_URL!, options)
+    .connect(process.env.MONGODB_URI!, options)
     // eslint-disable-next-line no-console
-    .then(() => console.log('Mongodb connected...'))
+    .then(() => console.log(`Mongodb connected to ${dbName} (NODE_ENV=${process.env.NODE_ENV})...`))
     // eslint-disable-next-line no-console
-    .catch((e) => console.log(e.message));
+    .catch((e) => console.log('Mongodb connection error', e.message));
 };
-
-mongoose.connection.on('connected', () => {
-  // eslint-disable-next-line no-console
-  console.log('Mongoose connected to db...');
-});
 
 mongoose.connection.on('disconnected', () => {
   // eslint-disable-next-line no-console
