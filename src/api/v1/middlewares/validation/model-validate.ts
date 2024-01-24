6f9d48d0edf1a3e8379149161e99bff16de3ba19
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-24 12:25:02 +0300

    refactor(validation): export simple model validate object

--- src/api/v1/middlewares/validation/model-validate.ts
+++ src/api/v1/middlewares/validation/model-validate.ts
@@ -1,23 +1,24 @@
-function ModelValidate() {}
-
-ModelValidate.prototype.url = (url: string) => {
+var url = (url: string) => {
   const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*$/i;
 
   return urlRegex.test(url);
 };
 
-ModelValidate.prototype.email = (email: string) => {
+var email = (email: string) => {
   const emailRegex =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return emailRegex.test(email);
 };
 
-ModelValidate.prototype.name = (name: string) => {
+var name = (name: string) => {
   const nameRegex = /^[a-zA-Zа-яА-ЯёЁ\s-]+$/;
 
   return nameRegex.test(name);
 };
 
-// @ts-ignore
-export var modelValidate = new ModelValidate();
+export var modelValidate = {
+  url,
+  email,
+  name,
+};
