d21a511ee1ebc6344c65845ca9cc2b611b13b17b
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-24 11:40:07 +0300

    feat(router): add /users routes to main router

--- src/api/v1/router.ts
+++ src/api/v1/router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { docRouter } from './docs';
+import { userRouter } from './user';
 
 var router = Router();
 export var BASE_PATH = '/api/v1';
 
 router.use('/docs', docRouter);
 router.get('/', (req, res) => {
-  res.send('Home Route');
+  res.json({ message: 'It works!' });
 });
+router.use('/users', userRouter);
 
 export { router };
