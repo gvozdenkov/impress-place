9e8e2cce91e31da9f6590e8e7288d9cbb8a4d6ab
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-26 09:22:40 +0300

    feat: add error handle middleware

--- src/api/v1/middlewares/index.ts
+++ src/api/v1/middlewares/index.ts
@@ -2,3 +2,4 @@ export { modelValidate } from './validation';
 export { promiseMiddleware } from './promise';
 export { handleResponse } from './handle-response';
 export { handleResponseError } from './handle-response-error';
+export { errorHandler } from './error-handler';
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,25 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import { BASE_PATH, connectDb, router } from '#api/v1';
-import { promiseMiddleware } from '#api/v1/middlewares';
+import { errorHandler, promiseMiddleware } from '#api/v1/middlewares';
+
+connectDb();
+
+var PORT = process.env.PORT || 3000;
 
 var app: Application = express();
 
 // @ts-ignore
 app.use(promiseMiddleware());
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDb();
-
 app.use(BASE_PATH, router);
 
-var PORT = process.env.PORT || 3000;
+// @ts-ignore
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
