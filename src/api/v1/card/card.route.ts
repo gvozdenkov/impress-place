1540dda4ca04c779fb783089529722a00924f900
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-29 00:09:52 +0300

    feat(route): add getById, deleteById to card.routes

--- src/api/v1/card/card.route.ts
+++ src/api/v1/card/card.route.ts
@@ -5,12 +5,20 @@ import { ModifiedResponse } from '../types';
 var cardRouter = Router();
 
 cardRouter.post('/', (req: Request, res: ModifiedResponse) => {
-  // console.log(req.user);
   var { name, link } = req.body;
   // eslint-disable-next-line no-underscore-dangle
   var owner = req.user._id;
   res.promise(cardService.create({ name, link, owner }));
 });
+
+cardRouter.delete('/:cardId', (req, res: ModifiedResponse) =>
+  res.promise(cardService.deleteById(req.params.cardId)),
+);
+
 cardRouter.get('/', (req, res: ModifiedResponse) => res.promise(cardService.getAll()));
+
+cardRouter.get('/:id', (req, res: ModifiedResponse) =>
+  res.promise(cardService.getById(req.params.id)),
+);
 
 export { cardRouter };
