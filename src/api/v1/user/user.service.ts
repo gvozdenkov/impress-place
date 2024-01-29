bc8ef25ede1c7041e143e90464ed7bc49e47bed2
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-29 14:11:01 +0300

    feat(service): add update user methods

--- src/api/v1/user/user.service.ts
+++ src/api/v1/user/user.service.ts
@@ -9,8 +9,30 @@ var create = ({ name, about, avatar }: UserSchema) => {
   return user.save();
 };
 
+var updateMe = async (id: string, { name, about }: UserSchema) =>
+  User.findByIdAndUpdate(
+    id,
+    { name, about },
+    {
+      returnDocument: 'after',
+      runValidators: true,
+    },
+  ).orFail();
+
+var updateMeAvatar = async (id: string, { avatar }: UserSchema) =>
+  User.findByIdAndUpdate(
+    id,
+    { avatar },
+    {
+      returnDocument: 'after',
+      runValidators: true,
+    },
+  ).orFail();
+
 export var userService = {
   create,
   getAll,
   getById,
+  updateMe,
+  updateMeAvatar,
 };
