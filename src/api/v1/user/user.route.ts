f16ea57855cbdfdfba90a817f4b2541e71178b7f
arty gvozdenkov <gvozdenkov@gmail.com>
2024-01-29 14:21:24 +0300

    feat(route): add update user routes

--- src/api/v1/user/user.route.ts
+++ src/api/v1/user/user.route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { Router } from 'express';
 import { userService } from './user.service';
 import { ModifiedResponse } from '../types';
@@ -9,5 +10,15 @@ userRouter.get('/', (req, res: ModifiedResponse) => res.promise(userService.getA
 userRouter.get('/:userId', (req, res: ModifiedResponse) =>
   res.promise(userService.getById(req.params.userId)),
 );
+
+userRouter.patch('/me', (req, res: ModifiedResponse) => {
+  var id = req.user._id;
+  res.promise(userService.updateMe(id, req.body));
+});
+
+userRouter.patch('/me/avatar', (req, res: ModifiedResponse) => {
+  var id = req.user._id;
+  res.promise(userService.updateMeAvatar(id, req.body));
+});
 
 export { userRouter };
