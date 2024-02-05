98ff8a65b30068f464658856dc6a8ec0f2a80feb
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-05 17:16:38 +0300

    refactor(user): create user controller

---
+++ src/v1/user/user.controller.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-underscore-dangle */
+import { Request } from 'express';
+import { ModifiedResponse } from '#v1/types';
+import { userService } from './user.service';
+
+var create = (req: Request, res: ModifiedResponse) => res.promise(userService.create(req.body));
+
+var getAll = (req: Request, res: ModifiedResponse) => res.promise(userService.getAll());
+
+var getById = (req: Request, res: ModifiedResponse) =>
+  res.promise(userService.getById(req.params.userId));
+
+var updateMe = (req: Request, res: ModifiedResponse) =>
+  res.promise(userService.updateMe(req.user._id, req.body));
+
+var updateMeAvatar = (req: Request, res: ModifiedResponse) =>
+  res.promise(userService.updateMeAvatar(req.user._id, req.body));
+
+export var userController = {
+  create,
+  getAll,
+  getById,
+  updateMe,
+  updateMeAvatar,
+};
--- src/v1/user/user.route.ts
+++ src/v1/user/user.route.ts
@@ -1,24 +1,12 @@
-/* eslint-disable no-underscore-dangle */
 import { Router } from 'express';
-import { userService } from './user.service';
-import { ModifiedResponse } from '../types';
+import { userController } from './user.controller';
 
 var userRouter = Router();
 
-userRouter.post('/', (req, res: ModifiedResponse) => res.promise(userService.create(req.body)));
-userRouter.get('/', (req, res: ModifiedResponse) => res.promise(userService.getAll()));
-userRouter.get('/:userId', (req, res: ModifiedResponse) =>
-  res.promise(userService.getById(req.params.userId)),
-);
-
-userRouter.patch('/me', (req, res: ModifiedResponse) => {
-  var id = req.user._id;
-  res.promise(userService.updateMe(id, req.body));
-});
-
-userRouter.patch('/me/avatar', (req, res: ModifiedResponse) => {
-  var id = req.user._id;
-  res.promise(userService.updateMeAvatar(id, req.body));
-});
+userRouter.post('/', userController.create);
+userRouter.get('/', userController.getAll);
+userRouter.get('/:userId', userController.getById);
+userRouter.patch('/me', userController.updateMe);
+userRouter.patch('/me/avatar', userController.updateMeAvatar);
 
 export { userRouter };
