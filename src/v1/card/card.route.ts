a3389dfc6bb6d7c639354a8379c9b149cd509eec
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-06 10:08:21 +0300

    refactor(card): add controller

--- src/v1/card/card.route.ts
+++ src/v1/card/card.route.ts
@@ -1,34 +1,16 @@
-/* eslint-disable no-underscore-dangle */
-import { Request, Router } from 'express';
-import { cardService } from './card.service';
-import { ModifiedResponse } from '../types';
+import { Router } from 'express';
+import { cardController } from './card.controller';
 
 var cardRouter = Router();
 
-cardRouter.post('/', (req: Request, res: ModifiedResponse) => {
-  var { name, link } = req.body;
-  var owner = req.user._id;
-  res.promise(cardService.create({ name, link, owner }));
-});
+cardRouter.post('/', cardController.create);
 
-cardRouter.put('/:cardId/likes', (req: Request, res: ModifiedResponse) => {
-  var userId = req.user._id;
-  res.promise(cardService.setLike(req.params.cardId, userId));
-});
+cardRouter.get('/', cardController.getAll);
+cardRouter.get('/:id', cardController.getById);
 
-cardRouter.delete('/:cardId/likes', (req: Request, res: ModifiedResponse) => {
-  var userId = req.user._id;
-  res.promise(cardService.removeLike(req.params.cardId, userId));
-});
+cardRouter.put('/:cardId/likes', cardController.setLike);
+cardRouter.delete('/:cardId/likes', cardController.removeLike);
 
-cardRouter.delete('/:cardId', (req, res: ModifiedResponse) =>
-  res.promise(cardService.deleteById(req.params.cardId)),
-);
-
-cardRouter.get('/', (req, res: ModifiedResponse) => res.promise(cardService.getAll()));
-
-cardRouter.get('/:id', (req, res: ModifiedResponse) =>
-  res.promise(cardService.getById(req.params.id)),
-);
+cardRouter.delete('/:cardId', cardController.deleteById);
 
 export { cardRouter };
