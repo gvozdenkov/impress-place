687c0c61393c4358a8d6d11aee1199795099ba56
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-07 07:32:40 +0300

    refactor(user): use helper to return data and success code

--- src/v1/user/user.service.ts
+++ src/v1/user/user.service.ts
@@ -1,5 +1,6 @@
 import { QueryOptions } from 'mongoose';
 import { ServiceReturnPromise } from '#v1/types';
+import { serviceReturn } from '#v1/helpers';
 import { User, UserSchema } from './user.model';
 
 var updateOptions: QueryOptions<UserSchema> = {
@@ -10,21 +11,18 @@ var updateOptions: QueryOptions<UserSchema> = {
 var create = async ({ name, about, avatar }: UserSchema): ServiceReturnPromise => {
   var user = new User({ name, about, avatar });
   var savedUser = await user.save();
-  return {
-    statusCode: 201,
-    data: savedUser,
-  };
+  return serviceReturn(savedUser, 201);
 };
 
-var getAll = async () => User.find({}).lean();
+var getAll = async () => serviceReturn(await User.find({}).lean());
 
-var getById = async (id: string) => User.findById(id).orFail();
+var getById = async (id: string) => serviceReturn(await User.findById(id).orFail());
 
 var updateMe = async (id: string, { name, about }: UserSchema) =>
-  User.findByIdAndUpdate(id, { name, about }, updateOptions).orFail();
+  serviceReturn(await User.findByIdAndUpdate(id, { name, about }, updateOptions).orFail());
 
 var updateMeAvatar = async (id: string, { avatar }: UserSchema) =>
-  User.findByIdAndUpdate(id, { avatar }, updateOptions).orFail();
+  serviceReturn(await User.findByIdAndUpdate(id, { avatar }, updateOptions).orFail());
 
 export var userService = {
   create,
