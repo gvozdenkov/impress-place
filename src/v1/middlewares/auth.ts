3cbb4e02013ff570c3355dde3c9cb5674878f0ec
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-11 15:55:20 +0300

    refactor: remove env var for test user

--- src/v1/middlewares/auth.ts
+++ src/v1/middlewares/auth.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export var auth = (req: Request, res: Response, next: NextFunction) => {
   req.user = {
     // temporal auth id for test user
-    _id: process.env.TEMP_USER_ID!,
+    _id: '65c0ed71f32fdf3c94c10381',
   };
 
   next();
