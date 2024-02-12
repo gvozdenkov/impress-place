700d6264fb9240df324513e384012fab7efa3f38
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-12 19:42:24 +0300

    chore: rename server var in server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import { config } from '#config';
 
 var { port: PORT, basePath: BASE_PATH } = config;
 
-var testServer = http.createServer(app);
+var server = http.createServer(app);
 
-testServer.listen(PORT, () => {
+server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}, http://localhost:${PORT}${BASE_PATH}`);
 });
