738526cc9107deb58a0a12a5386da49dd5b2cd7e
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-13 07:48:21 +0300

    feat: add invalid email message

--- src/messages.ts
+++ src/messages.ts
@@ -1,6 +1,7 @@
 var minLength = (v: number) => `Must be at least ${v} characters`;
 var maxLength = (v: number) => `Should be maximum ${v} characters`;
 var invalidUrl = () => 'this is not valid url';
+var invalidEmail = () => 'this is not valid email';
 var invalidName = () => 'You can only use Latin and Russian letters, hyphens and spaces';
 var internalServerError = () => 'Internal server error';
 var notFound = (v: string) => `${v} not found`;
@@ -19,6 +20,7 @@ export var message = {
   maxLength,
   invalidUrl,
   invalidName,
+  invalidEmail,
   internalServerError,
   notFound,
   findOneAndDeleteError,
