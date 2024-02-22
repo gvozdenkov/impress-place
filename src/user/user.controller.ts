0776bd60de37a734164d2d2b03a12351e2ddd629
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-22 11:13:38 +0300

    refactor: use http-status for status codes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,31 @@
 /* eslint-disable no-underscore-dangle */
 import { Request, Response } from 'express';
+import httpStatus from 'http-status';
 import { catchAsync, formatResponseData } from '#utils';
 import { userService } from '#services';
 
 var getAll = catchAsync(async (req: Request, res: Response) => {
   var users = await userService.getAll();
 
-  res.status(200).send(formatResponseData(users));
+  res.status(httpStatus.OK).send(formatResponseData(users));
 });
 
 var getById = catchAsync(async (req: Request, res: Response) => {
   var user = await userService.getById(req.params.userId);
 
-  res.status(200).send(formatResponseData(user));
+  res.status(httpStatus.OK).send(formatResponseData(user));
 });
 
 var updateMe = catchAsync(async (req: Request, res: Response) => {
   var updatedUser = await userService.updateMe(req.user._id, req.body);
 
-  res.status(200).send(formatResponseData(updatedUser));
+  res.status(httpStatus.OK).send(formatResponseData(updatedUser));
 });
 
 var updateMeAvatar = catchAsync(async (req: Request, res: Response) => {
   var updatedAvatar = await userService.updateMeAvatar(req.user._id, req.body);
 
-  res.status(200).send(formatResponseData(updatedAvatar));
+  res.status(httpStatus.OK).send(formatResponseData(updatedAvatar));
 });
 
 export var userController = {
