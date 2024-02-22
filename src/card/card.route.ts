7a1455bb89fb820ac41b3dd9971c3d2594d15862
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-22 11:15:23 +0300

    refactor(card): route use sible handleLike to PUT & DELETE

--- src/card/card.route.ts
+++ src/card/card.route.ts
@@ -8,8 +8,8 @@ cardRouter.post('/', cardController.create);
 cardRouter.get('/', cardController.getAll);
 cardRouter.get('/:id', cardController.getById);
 
-cardRouter.put('/:cardId/likes', cardController.setLike);
-cardRouter.delete('/:cardId/likes', cardController.removeLike);
+cardRouter.put('/:cardId/likes', cardController.handleLike);
+cardRouter.delete('/:cardId/likes', cardController.handleLike);
 
 cardRouter.delete('/:cardId', cardController.deleteById);
 
