204fb5d9b345eb11bb1e7dc318ab1a8fa0a8c1b8
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-22 11:12:05 +0300

    feat: add create & getByEmail methods to user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,7 @@
 import { QueryOptions } from 'mongoose';
+import httpStatus from 'http-status';
+import { ApiError } from '#utils';
+import { message } from '#messages';
 import { User, UserSchema } from '../user/user.model';
 
 var updateOptions: QueryOptions<UserSchema> = {
@@ -6,9 +9,19 @@ var updateOptions: QueryOptions<UserSchema> = {
   runValidators: true,
 };
 
+var create = async ({ name, about, avatar, email, password }: UserSchema) => {
+  if (await User.isEmailTaken(email)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, message.existsEmail(email));
+  }
+
+  return User.create({ name, about, avatar, email, password });
+};
+
 var getAll = async () => await User.find({}).orFail();
 
 var getById = async (id: string) => await User.findById(id).orFail();
+
+var getByEmail = async (email: string) => await User.findOne({ email });
 
 var updateMe = async (id: string, { name, about }: UserSchema) =>
   await User.findByIdAndUpdate(id, { name, about }, updateOptions).orFail();
@@ -17,8 +30,10 @@ var updateMeAvatar = async (id: string, { avatar }: UserSchema) =>
   await User.findByIdAndUpdate(id, { avatar }, updateOptions).orFail();
 
 export var userService = {
+  create,
   getAll,
   getById,
+  getByEmail,
   updateMe,
   updateMeAvatar,
 };
