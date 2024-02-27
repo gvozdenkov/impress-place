5a8cafabda0a4d6fe002d08c606e47c4da9f6cde
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-27 12:04:07 +0300

    fix(user): route order

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
+import { auth } from '#middlewares';
 import { userController } from './user.controller';
 
 var userRouter = Router();
 
 userRouter.get('/', userController.getAll);
+userRouter.get('/me', auth, userController.getMe);
 userRouter.get('/:userId', userController.getById);
-userRouter.patch('/me', userController.updateMe);
 userRouter.patch('/me/avatar', userController.updateMeAvatar);
+userRouter.patch('/me', userController.updateMe);
 
 export { userRouter };
