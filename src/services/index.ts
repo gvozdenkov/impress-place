f82905107c826e8f4d0bc1853690129a47bdea38
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-27 11:56:44 +0300

    refactor(move): userService to /user eniti

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,2 @@
-export { userService } from './user.service';
 export { cookieService } from './cookie.service';
 export { tokenService } from './token.service';
-export { authService } from './auth.service';
