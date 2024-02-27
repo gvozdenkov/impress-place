fbcf50816d0be2777dfb313339ddcad3f27f2828
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-27 11:55:43 +0300

    refactor(auth): controller use authService

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,30 +1,31 @@
 /* eslint-disable no-underscore-dangle */
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-import { catchAsync, formatResponseData } from '#utils';
-import { authService, cookieService, tokenService, userService } from '#services';
+import { catchAsync, formatResponseData, getExpiresInTime } from '#utils';
 import { config } from '#config';
+import { cookieService, tokenService } from '#services';
+import { authService } from './auth.service';
 
-var { secret, accessExpired } = config.jwt;
+var { secret, accessExpiresIn } = config.jwt;
 
 var login = catchAsync(async (req: Request, res: Response) => {
   var user = await authService.login(req.body);
   // @ts-ignore
-  var accessToken = tokenService.generate(user._id, secret);
+  var accessToken = tokenService.generate(user._id, secret, accessExpiresIn);
 
-  cookieService.setTokenCookie(res, 'accessToken', accessToken, accessExpired);
+  cookieService.setTokenCookie(res, 'accessToken', accessToken, getExpiresInTime(accessExpiresIn));
 
-  res.status(200).send(formatResponseData(user));
+  res.status(200).json(formatResponseData(user));
 });
 
 var register = catchAsync(async (req: Request, res: Response) => {
-  var newUser = await userService.create(req.body);
+  var newUser = await authService.create(req.body);
 
-  var accessToken = tokenService.generate(newUser._id, secret);
+  var accessToken = tokenService.generate(newUser._id, secret, accessExpiresIn);
 
-  cookieService.setTokenCookie(res, 'accessToken', accessToken, accessExpired);
+  cookieService.setTokenCookie(res, 'accessToken', accessToken, getExpiresInTime(accessExpiresIn));
 
-  res.status(httpStatus.CREATED).send(formatResponseData(newUser));
+  res.status(httpStatus.CREATED).json(formatResponseData(newUser));
 });
 
 export var authController = {
