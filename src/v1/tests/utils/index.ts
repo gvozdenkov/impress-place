dd2bfa1af78c370fec36cc88a3415cddc450231f
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-28 10:15:36 +0300

    refactor(rename): .env jwt token expires vars

---
+++ src/v1/tests/utils/index.ts
@@ -0,0 +1,3 @@
+export { getReqCookies } from './get-header-cookies';
+export { setupTestDB, testDefaultUser } from './setup-test-db';
+export { createUser, mapToTestDTO, randomeUser } from './user-dto';
