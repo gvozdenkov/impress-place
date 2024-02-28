414216f0c9530cb5bd176e4c43c5ac13dc09b4fe
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-28 10:17:29 +0300

    test(user): refactor for auth before access endpoints

--- src/v1/tests/users.test.ts
+++ src/v1/tests/users.test.ts
@@ -5,55 +5,77 @@ import request from 'supertest';
 import { app } from '#app';
 import { basePathV1 } from '#v1';
 import { message } from '#messages';
-import { createUser, mapToTestDTO } from './utils/user-dto';
-import { setupTestDB } from './utils/setup-test-db';
+import { User, UserDocument } from '#user';
+import { createUser, getReqCookies, mapToTestDTO, setupTestDB, testDefaultUser } from './utils';
 
 var basePath = `${basePathV1}/users`;
+var logedInUser: UserDocument;
 
 setupTestDB();
 
 describe('User service', () => {
   // GET ========================================
   describe('Get all users', () => {
+    beforeEach((done) => {
+      User.create(testDefaultUser).then((user) => {
+        logedInUser = user;
+        done();
+      });
+    });
     it('Should return all users', async () => {
+      var { email, password } = testDefaultUser;
       var user1 = await createUser();
       var user2 = await createUser();
 
+      var cookies = await getReqCookies({ email, password });
+
       await request(app)
         .get(`${basePath}`)
+        .set('Cookie', [...cookies])
         .expect(200)
         .expect('Content-type', /json/)
         .then((res) => {
           assert.deepStrictEqual(res.body, {
             status: 'success',
-            data: [mapToTestDTO(user1), mapToTestDTO(user2)],
+            data: [mapToTestDTO(logedInUser), mapToTestDTO(user1), mapToTestDTO(user2)],
           });
         });
     });
   });
   describe(`Get user by id`, () => {
+    beforeEach((done) => {
+      User.create(testDefaultUser).then((user) => {
+        logedInUser = user;
+        done();
+      });
+    });
     it('Should return user by id', async () => {
-      var user = await createUser();
-      var _id = user._id.toString();
+      var { email, password } = testDefaultUser;
+      var _id = logedInUser._id.toString();
+
+      var cookies = await getReqCookies({ email, password });
 
       await request(app)
         .get(`${basePath}/${_id}`)
+        .set('Cookie', [...cookies])
         .expect(200)
         .expect('Content-type', /json/)
         .then((res) => {
           assert.deepStrictEqual(res.body, {
             status: 'success',
-            data: mapToTestDTO(user),
+            data: mapToTestDTO(logedInUser),
           });
         });
     });
     it('Should throw 404 "User not found" for wrong id', async () => {
-      await createUser();
+      var { email, password } = testDefaultUser;
 
+      var cookies = await getReqCookies({ email, password });
       var wrongId = 'someWrongId123';
 
       await request(app)
         .get(`${basePath}/${wrongId}`)
+        .set('Cookie', [...cookies])
         .expect(404)
         .expect('Content-type', /json/)
         .then((res) => {
