e82dc4148a21af63aed734f373a07e596561a72f
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-28 07:23:39 +0300

    test(user): remove create user tests, use basePathV1

--- src/v1/tests/users.test.ts
+++ src/v1/tests/users.test.ts
@@ -2,21 +2,19 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { assert } from 'chai';
 import request from 'supertest';
-import { config } from '#config';
 import { app } from '#app';
+import { basePathV1 } from '#v1';
 import { message } from '#messages';
-import { USER } from '#user';
-import { randomeString } from '#utils';
-import { createUser, mapToTestDTO, randomeUser } from './utils/user-dto';
+import { createUser, mapToTestDTO } from './utils/user-dto';
 import { setupTestDB } from './utils/setup-test-db';
 
-var basePath = `${config.basePath}/users`;
+var basePath = `${basePathV1}/users`;
 
 setupTestDB();
 
-describe(basePath, () => {
+describe('User service', () => {
   // GET ========================================
-  describe(`GET ${basePath}`, () => {
+  describe('Get all users', () => {
     it('Should return all users', async () => {
       var user1 = await createUser();
       var user2 = await createUser();
@@ -33,7 +31,7 @@ describe(basePath, () => {
         });
     });
   });
-  describe(`GET ${basePath}/:userId`, () => {
+  describe(`Get user by id`, () => {
     it('Should return user by id', async () => {
       var user = await createUser();
       var _id = user._id.toString();
@@ -52,135 +50,16 @@ describe(basePath, () => {
     it('Should throw 404 "User not found" for wrong id', async () => {
       await createUser();
 
-      var _id = 'someWrongId123';
+      var wrongId = 'someWrongId123';
 
       await request(app)
-        .get(`${basePath}/${_id}`)
+        .get(`${basePath}/${wrongId}`)
         .expect(404)
         .expect('Content-type', /json/)
         .then((res) => {
           assert.deepStrictEqual(res.body, {
             status: 'fail',
             message: message.notFound('user'),
-          });
-        });
-    });
-  });
-
-  // POST ========================================
-  describe(`POST ${basePath}`, () => {
-    it(`Should throw 400 error with name longer then ${USER.nameMaxLength}`, async () => {
-      var invalidLength = USER.nameMaxLength + 1;
-      var invalidName = randomeString(invalidLength, invalidLength, ' -');
-      var user = randomeUser({ name: invalidName });
-
-      await request(app)
-        .post(`${basePath}`)
-        .send({ ...user })
-        .expect(400)
-        .expect('Content-type', /json/)
-        .then((res) => {
-          assert.deepStrictEqual(res.body, {
-            status: 'fail',
-            message: message.validationFailed(
-              'user',
-              'name',
-              message.maxLength(USER.nameMaxLength),
-            ),
-          });
-        });
-    });
-    it(`Should throw 400 error with name shorter then ${USER.nameMinLength}`, async () => {
-      var invalidLength = USER.nameMinLength - 1;
-      var invalidName = randomeString(invalidLength, invalidLength, ' -');
-      var user = randomeUser({ name: invalidName });
-
-      await request(app)
-        .post(`${basePath}`)
-        .send({ ...user })
-        .expect(400)
-        .expect('Content-type', /json/)
-        .then((res) => {
-          assert.deepStrictEqual(res.body, {
-            status: 'fail',
-            message: message.validationFailed(
-              'user',
-              'name',
-              message.minLength(USER.nameMinLength),
-            ),
-          });
-        });
-    });
-    it(`Should throw 400 error with name have invalid cheracters`, async () => {
-      var invalidName = `${randomeString(USER.nameMinLength, USER.nameMaxLength)}+`;
-      var user = randomeUser({ name: invalidName });
-
-      await request(app)
-        .post(`${basePath}`)
-        .send({ ...user })
-        .expect(400)
-        .expect('Content-type', /json/)
-        .then((res) => {
-          assert.deepStrictEqual(res.body, {
-            status: 'fail',
-            message: message.validationFailed('user', 'name', message.invalidName()),
-          });
-        });
-    });
-    it(`Should throw 400 error with about longer then ${USER.aboutMaxLength}`, async () => {
-      var invalidLength = USER.aboutMaxLength + 1;
-      var invalidAbout = randomeString(invalidLength, invalidLength);
-      var user = randomeUser({ about: invalidAbout });
-
-      await request(app)
-        .post(`${basePath}`)
-        .send({ ...user })
-        .expect(400)
-        .expect('Content-type', /json/)
-        .then((res) => {
-          assert.deepStrictEqual(res.body, {
-            status: 'fail',
-            message: message.validationFailed(
-              'user',
-              'about',
-              message.maxLength(USER.aboutMaxLength),
-            ),
-          });
-        });
-    });
-    it(`Should throw 400 error with about shorter then ${USER.aboutMinLength}`, async () => {
-      var invalidLength = USER.aboutMinLength - 1;
-      var invalidAbout = randomeString(invalidLength, invalidLength);
-      var user = randomeUser({ about: invalidAbout });
-
-      await request(app)
-        .post(`${basePath}`)
-        .send({ ...user })
-        .expect(400)
-        .expect('Content-type', /json/)
-        .then((res) => {
-          assert.deepStrictEqual(res.body, {
-            status: 'fail',
-            message: message.validationFailed(
-              'user',
-              'about',
-              message.minLength(USER.aboutMinLength),
-            ),
-          });
-        });
-    });
-    it('Should throw 400 error with invalid avatar url', async () => {
-      var user = randomeUser({ avatar: 'invalid url' });
-
-      await request(app)
-        .post(`${basePath}`)
-        .send({ ...user })
-        .expect(400)
-        .expect('Content-type', /json/)
-        .then((res) => {
-          assert.deepStrictEqual(res.body, {
-            status: 'fail',
-            message: message.validationFailed('user', 'avatar', message.invalidUrl()),
           });
         });
     });
