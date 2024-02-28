9cbfbd45808bb5a5d9a8e1413e6058c65273a149
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-28 07:24:31 +0300

    test(user): add invalid input tests, use basePathV1

--- src/v1/tests/register.test.ts
+++ src/v1/tests/register.test.ts
@@ -2,137 +2,253 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { assert } from 'chai';
 import request from 'supertest';
-import { config } from '#config';
 import { app } from '#app';
+import { basePathV1 } from '#v1';
 import { message } from '#messages';
 import { USER } from '#user';
+import { randomeString } from '#utils';
 import { createUser, randomeUser } from './utils/user-dto';
 import { setupTestDB } from './utils/setup-test-db';
 
-var { basePath } = config;
-
-var basePathV1 = `${basePath}/v1`;
 var registerPath = `${basePathV1}/signup`;
 
 setupTestDB();
+describe('Auth Service', () => {
+  describe('Register new user', () => {
+    it("Should create new user with requied 'email', 'password', 'name', 'about', 'avatar'", async () => {
+      var user = randomeUser();
+      var { name, about, avatar, email } = user;
 
-describe('Register new user', () => {
-  it("Should create new user with requied 'email', 'password', 'name', 'about', 'avatar'", async () => {
-    var user = randomeUser();
-    var { name, about, avatar, email } = user;
+      await request(app)
+        .post(registerPath)
+        .send({ ...user })
+        .expect(201)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          var { status, data } = res.body;
 
-    await request(app)
-      .post(registerPath)
-      .send({ ...user })
-      .expect(201)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        var { status, data } = res.body;
-
-        assert.equal(status, 'success');
-        assert.include(data, { name, about, avatar, email });
-      });
-  });
-  it("Should create new user with 'email' and 'password' only", async () => {
-    var user = randomeUser();
-    var { email, password } = user;
-
-    await request(app)
-      .post(registerPath)
-      .send({ email, password })
-      .expect(201)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        var { status, data } = res.body;
-
-        assert.equal(status, 'success');
-        assert.include(data, {
-          name: USER.nameDefault,
-          about: USER.aboutDefault,
-          avatar: USER.avatarDefault,
-          email,
+          assert.equal(status, 'success');
+          assert.include(data, { name, about, avatar, email });
         });
-      });
-  });
-  it('Created user should not contain password field', async () => {
-    var user = randomeUser();
+    });
+    it("Should create new user with 'email' and 'password' only", async () => {
+      var user = randomeUser();
+      var { email, password } = user;
 
-    await request(app)
-      .post(registerPath)
-      .send({ ...user })
-      .expect(201)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        var { data } = res.body;
+      await request(app)
+        .post(registerPath)
+        .send({ email, password })
+        .expect(201)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          var { status, data } = res.body;
 
-        assert.notProperty(data, 'password');
-      });
-  });
-  it('Should create new user without name (with default name)', async () => {
-    var user = randomeUser();
-    var { about, avatar, email, password } = user;
-
-    await request(app)
-      .post(registerPath)
-      .send({ about, avatar, email, password })
-      .expect(201)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        var { status, data } = res.body;
-
-        assert.equal(status, 'success');
-        assert.include(data, { name: USER.nameDefault, about, avatar, email });
-      });
-  });
-
-  it('Should create new user without about (with default about)', async () => {
-    var user = randomeUser();
-    var { name, avatar, email, password } = user;
-
-    await request(app)
-      .post(registerPath)
-      .send({ name, avatar, email, password })
-      .expect(201)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        var { status, data } = res.body;
-
-        assert.equal(status, 'success');
-        assert.include(data, { name, about: USER.aboutDefault, avatar, email });
-      });
-  });
-
-  it('Should create new user without avatar (with default avatar)', async () => {
-    var user = randomeUser();
-    var { name, about, email, password } = user;
-
-    await request(app)
-      .post(registerPath)
-      .send({ name, about, email, password })
-      .expect(201)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        var { status, data } = res.body;
-
-        assert.equal(status, 'success');
-        assert.include(data, { name, about, avatar: USER.avatarDefault, email });
-      });
-  });
-  it('Should not create a new user if email already exists', async () => {
-    var user = randomeUser();
-    await createUser({ ...user });
-    var { email } = user;
-
-    await request(app)
-      .post(registerPath)
-      .send({ ...user })
-      .expect(409)
-      .expect('Content-type', /json/)
-      .then((res) => {
-        assert.deepStrictEqual(res.body, {
-          status: 'fail',
-          message: message.existsEmail(email),
+          assert.equal(status, 'success');
+          assert.include(data, {
+            name: USER.nameDefault,
+            about: USER.aboutDefault,
+            avatar: USER.avatarDefault,
+            email,
+          });
         });
-      });
+    });
+    it('Created user should not contain password field', async () => {
+      var user = randomeUser();
+
+      await request(app)
+        .post(registerPath)
+        .send({ ...user })
+        .expect(201)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          var { data } = res.body;
+
+          assert.notProperty(data, 'password');
+        });
+    });
+    it('Should create new user without name (with default name)', async () => {
+      var user = randomeUser();
+      var { about, avatar, email, password } = user;
+
+      await request(app)
+        .post(registerPath)
+        .send({ about, avatar, email, password })
+        .expect(201)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          var { status, data } = res.body;
+
+          assert.equal(status, 'success');
+          assert.include(data, { name: USER.nameDefault, about, avatar, email });
+        });
+    });
+
+    it('Should create new user without about (with default about)', async () => {
+      var user = randomeUser();
+      var { name, avatar, email, password } = user;
+
+      await request(app)
+        .post(registerPath)
+        .send({ name, avatar, email, password })
+        .expect(201)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          var { status, data } = res.body;
+
+          assert.equal(status, 'success');
+          assert.include(data, { name, about: USER.aboutDefault, avatar, email });
+        });
+    });
+
+    it('Should create new user without avatar (with default avatar)', async () => {
+      var user = randomeUser();
+      var { name, about, email, password } = user;
+
+      await request(app)
+        .post(registerPath)
+        .send({ name, about, email, password })
+        .expect(201)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          var { status, data } = res.body;
+
+          assert.equal(status, 'success');
+          assert.include(data, { name, about, avatar: USER.avatarDefault, email });
+        });
+    });
+
+    // ==================== Fail create new user ====================
+    it('If email already exists, should fail to create a new user', async () => {
+      var user = randomeUser();
+      await createUser({ ...user });
+      var { email } = user;
+
+      await request(app)
+        .post(registerPath)
+        .send({ ...user })
+        .expect(409)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          assert.deepStrictEqual(res.body, {
+            status: 'fail',
+            message: message.existsEmail(email),
+          });
+        });
+    });
+    it(`If 'name' longer then ${USER.nameMaxLength}, should fail to create a new user`, async () => {
+      var invalidLength = USER.nameMaxLength + 1;
+      var invalidName = randomeString(invalidLength, invalidLength);
+      var user = randomeUser({ name: invalidName });
+
+      await request(app)
+        .post(`${registerPath}`)
+        .send({ ...user })
+        .expect(400)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          assert.deepStrictEqual(res.body, {
+            status: 'fail',
+            message: message.validationFailed(
+              'user',
+              'name',
+              message.maxLength(USER.nameMaxLength),
+            ),
+          });
+        });
+    });
+    it(`If 'name' shorter then ${USER.nameMinLength}, should fail to create a new user`, async () => {
+      var invalidLength = USER.nameMinLength - 1;
+      var invalidName = randomeString(invalidLength, invalidLength, ' -');
+      var user = randomeUser({ name: invalidName });
+
+      await request(app)
+        .post(`${registerPath}`)
+        .send({ ...user })
+        .expect(400)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          assert.deepStrictEqual(res.body, {
+            status: 'fail',
+            message: message.validationFailed(
+              'user',
+              'name',
+              message.minLength(USER.nameMinLength),
+            ),
+          });
+        });
+    });
+    it(`If 'name' have invalid cheracters, should fail to create a new user`, async () => {
+      var invalidName = `${randomeString(USER.nameMinLength, USER.nameMaxLength)}+`;
+      var user = randomeUser({ name: invalidName });
+
+      await request(app)
+        .post(`${registerPath}`)
+        .send({ ...user })
+        .expect(400)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          assert.deepStrictEqual(res.body, {
+            status: 'fail',
+            message: message.validationFailed('user', 'name', message.invalidName()),
+          });
+        });
+    });
+    it(`If 'about' longer then ${USER.aboutMaxLength}, should fail to create a new user`, async () => {
+      var invalidLength = USER.aboutMaxLength + 1;
+      var invalidAbout = randomeString(invalidLength, invalidLength);
+      var user = randomeUser({ about: invalidAbout });
+
+      await request(app)
+        .post(`${registerPath}`)
+        .send({ ...user })
+        .expect(400)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          assert.deepStrictEqual(res.body, {
+            status: 'fail',
+            message: message.validationFailed(
+              'user',
+              'about',
+              message.maxLength(USER.aboutMaxLength),
+            ),
+          });
+        });
+    });
+    it(`If 'about' shorter then ${USER.aboutMinLength}, should fail to create a new user`, async () => {
+      var invalidLength = USER.aboutMinLength - 1;
+      var invalidAbout = randomeString(invalidLength, invalidLength);
+      var user = randomeUser({ about: invalidAbout });
+
+      await request(app)
+        .post(`${registerPath}`)
+        .send({ ...user })
+        .expect(400)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          assert.deepStrictEqual(res.body, {
+            status: 'fail',
+            message: message.validationFailed(
+              'user',
+              'about',
+              message.minLength(USER.aboutMinLength),
+            ),
+          });
+        });
+    });
+    it("If invalid 'avatar' url, should fail to create a new user", async () => {
+      var user = randomeUser({ avatar: 'invalid url' });
+
+      await request(app)
+        .post(`${registerPath}`)
+        .send({ ...user })
+        .expect(400)
+        .expect('Content-type', /json/)
+        .then((res) => {
+          assert.deepStrictEqual(res.body, {
+            status: 'fail',
+            message: message.validationFailed('user', 'avatar', message.invalidUrl()),
+          });
+        });
+    });
   });
 });
