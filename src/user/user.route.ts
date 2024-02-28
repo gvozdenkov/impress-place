be927a4acdff9a5ad28457e266d21c0fe25bb7d5
arty gvozdenkov <gvozdenkov@gmail.com>
2024-02-28 07:31:42 +0300

    feat(user): apply auth middleware to whole user router

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { auth } from '#middlewares';
 import { userController } from './user.controller';
 
 var userRouter = Router();
 
 userRouter.get('/', userController.getAll);
-userRouter.get('/me', auth, userController.getMe);
+userRouter.get('/me', userController.getMe);
 userRouter.get('/:userId', userController.getById);
 userRouter.patch('/me/avatar', userController.updateMeAvatar);
 userRouter.patch('/me', userController.updateMe);
