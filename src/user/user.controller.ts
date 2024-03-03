15ad150cc2146c4757cd6c4219463ffccd48a072
arty gvozdenkov <gvozdenkov@gmail.com>
2024-03-03 19:29:03 +0300

    refactor: replace res.send -> res.json

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,30 +7,30 @@ import { userService } from './user.service';
 var getAll = catchAsync(async (req: Request, res: Response) => {
   var users = await userService.getAll();
 
-  res.status(httpStatus.OK).send(formatResponseData(users));
+  res.status(httpStatus.OK).json(formatResponseData(users));
 });
 
 var getById = catchAsync(async (req: Request, res: Response) => {
   var user = await userService.getById(req.params.userId);
 
-  res.status(httpStatus.OK).send(formatResponseData(user));
+  res.status(httpStatus.OK).json(formatResponseData(user));
 });
 
 var getMe = catchAsync(async (req: Request, res: Response) => {
   var user = await userService.getById(req.userId);
-  res.status(httpStatus.OK).send(formatResponseData(user));
+  res.status(httpStatus.OK).json(formatResponseData(user));
 });
 
 var updateMe = catchAsync(async (req: Request, res: Response) => {
   var updatedUser = await userService.updateMe(req.userId, req.body);
 
-  res.status(httpStatus.OK).send(formatResponseData(updatedUser));
+  res.status(httpStatus.OK).json(formatResponseData(updatedUser));
 });
 
 var updateMeAvatar = catchAsync(async (req: Request, res: Response) => {
   var updatedAvatar = await userService.updateMeAvatar(req.userId, req.body);
 
-  res.status(httpStatus.OK).send(formatResponseData(updatedAvatar));
+  res.status(httpStatus.OK).json(formatResponseData(updatedAvatar));
 });
 
 export var userController = {
