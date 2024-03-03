e356806c89dbca0648cca5cac8842e723f115bba
arty gvozdenkov <gvozdenkov@gmail.com>
2024-03-03 19:25:38 +0300

    feat(card): add auth middleware before card actions routes

--- src/card/card.route.ts
+++ src/card/card.route.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
+import { auth } from '#middlewares';
 import { cardController } from './card.controller';
 
 var cardRouter = Router();
 
-cardRouter.post('/', cardController.create);
-
 cardRouter.get('/', cardController.getAll);
 cardRouter.get('/:id', cardController.getById);
 
+cardRouter.use(auth);
+
+cardRouter.post('/', cardController.create);
 cardRouter.put('/:cardId/likes', cardController.handleLike);
 cardRouter.delete('/:cardId/likes', cardController.handleLike);
-
 cardRouter.delete('/:cardId', cardController.deleteById);
 
 export { cardRouter };
