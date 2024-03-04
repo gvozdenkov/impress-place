6aefa819542c4df524777f19b77ea2259abab2e7
arty gvozdenkov <gvozdenkov@gmail.com>
2024-03-04 17:23:35 +0300

    feat: add request Joi validation to user, card, auth routes

--- src/auth/auth.route.ts
+++ src/auth/auth.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { schemaValidator } from '#middlewares';
+import { createUser, loginUser } from '#user/validation-schemas';
 import { authController } from './auth.controller';
 
 var authRouter = Router();
 
-authRouter.post('/signup', authController.register);
-authRouter.post('/signin', authController.login);
+authRouter.post('/signup', schemaValidator(createUser), authController.register);
+authRouter.post('/signin', schemaValidator(loginUser), authController.login);
 
 export { authRouter };
